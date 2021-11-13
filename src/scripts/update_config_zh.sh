55175183a059e2d77f1ac2aa58d950634f3c6fc5
Newbe36524 <newbe36524@qq.com>
2021-11-13 20:59:46 +0800

    Add github action to test build (#93)
    * permission issue
    
    * ?
    
    * 777
    
    * no file mode
    
    * Update submodules
    
    * Update submodules
    
    * Update submodules

---
+++ src/scripts/update_config_zh.sh
@@ -0,0 +1,9 @@
+sed -i 's/..\/translations\/docs-zh\/content\/zh-hans/..\/translated_content\/zh_CN\/content/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/contributing/..\/translated_content\/zh_CN\/contributing/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/sdks_python/..\/translated_content\/zh_CN\/sdks_python/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/sdks_php/..\/translated_content\/zh_CN\/sdks_php/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/sdks_dotnet/..\/translated_content\/zh_CN\/sdks_dotnet/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/sdks_java/..\/translated_content\/zh_CN\/sdks_java/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/sdks_go/..\/translated_content\/zh_CN\/sdks_go/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/sdks_js/..\/translated_content\/zh_CN\/sdks_js/g' config.toml
+sed -i 's/languageCode = "en-us"/defaultContentLanguage = "zh-hans"/g' config.toml
