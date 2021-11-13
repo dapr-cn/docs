557e14b3a9bff7dfea016a7d72cf7909cad0c2e0
newbe36524 <newbe36524@qq.com>
2021-11-13 20:25:46 +0800

    Update content for v1.0

--- daprdocs/update_config_zh.sh
+++ daprdocs/update_config_zh.sh
@@ -5,4 +5,5 @@ sed -i 's/..\/translations\/docs-zh\/content\/sdks_php/..\/translated_content\/z
 sed -i 's/..\/translations\/docs-zh\/content\/sdks_dotnet/..\/translated_content\/zh_CN\/sdks_dotnet/g' config.toml
 sed -i 's/..\/translations\/docs-zh\/content\/sdks_java/..\/translated_content\/zh_CN\/sdks_java/g' config.toml
 sed -i 's/..\/translations\/docs-zh\/content\/sdks_go/..\/translated_content\/zh_CN\/sdks_go/g' config.toml
-sed -i 's/languageCode = "en-us"/defaultContentLanguage = "zh-hans"/g' config.toml
+sed -i 's/..\/translations\/docs-zh\/content\/sdks_js/..\/translated_content\/zh_CN\/sdks_js/g' config.toml
+sed -i 's/languageCode = "en-us"/defaultContentLanguage = "zh-hans"/g' config.tomlinv --list
